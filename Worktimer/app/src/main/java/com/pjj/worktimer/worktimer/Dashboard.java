b9c19ffd9773887e5f361b03e4cfab453629eb46
Jens <ichunddu180@hotmail.de>
2017-05-04 22:12:37 +0200

    adding Projects (not working)

--- Worktimer/app/src/main/java/com/pjj/worktimer/worktimer/Dashboard.java
+++ Worktimer/app/src/main/java/com/pjj/worktimer/worktimer/Dashboard.java
@@ -2,10 +2,12 @@ package com.pjj.worktimer.worktimer;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 
 /**
@@ -13,17 +15,21 @@ import android.view.ViewGroup;
  */
 public class Dashboard extends Fragment {
 
+    private ProjectLayout pl;
 
-    public Dashboard() {
-        // Required empty public constructor
+    public Dashboard(){
+
     }
 
+    public ProjectLayout getProjectLayout(){
+        return pl;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
-    }
-
+        pl = new ProjectLayout(getContext());
+        return pl;
+    };
 }
--- Worktimer/app/src/main/java/com/pjj/worktimer/worktimer/main.java
+++ Worktimer/app/src/main/java/com/pjj/worktimer/worktimer/main.java
@@ -1,21 +1,31 @@
 package com.pjj.worktimer.worktimer;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class main extends AppCompatActivity {
 
-    Toolbar toolbar;
-    TabLayout tabLayout;
-    ViewPager viewPager;
-    ViewPagerAdapter vpa;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter vpa;
+    private Dashboard dashboard;
+    private ProjectLayout pl;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //TextView logo = (TextView) findViewById(R.id.Logo);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
@@ -23,11 +33,29 @@ public class main extends AppCompatActivity {
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         vpa = new ViewPagerAdapter(getSupportFragmentManager());
         vpa.addFragment("Statistik", new Statistik());
-        vpa.addFragment("Dashboard", new Dashboard());
+        dashboard = new Dashboard();
+        pl = dashboard.getProjectLayout();
+        vpa.addFragment("Dashboard", dashboard);
         vpa.addFragment("Einstellungen", new Einstellungen());
         viewPager.setAdapter(vpa);
         tabLayout.setupWithViewPager(viewPager);
         TabLayout.Tab firstTab = tabLayout.getTabAt(1);
         firstTab.select();
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(onclick());
+    }
+
+    public View.OnClickListener onclick(){
+        return(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"drawProjects",Toast.LENGTH_SHORT).show();
+                pl.addProject();
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
